f8d8f43ff392afdcec9166960fdde312688d9ff7
William Jesus <william@william.local>
2022-11-27 23:53:34 -0300

    Change sequelize to pg module

--- database.js
+++ database.js
@@ -1,7 +1,8 @@
 const { Sequelize } = require('sequelize')
+const pg = require('pg')
 
 const HOST_DATABASE = process.env.HOST_DATABASE
 
-const sequelize = new Sequelize(HOST_DATABASE, { dialect: 'postgres' })
+const sequelize = new Sequelize(HOST_DATABASE, { dialectModule: pg })
  
 module.exports = sequelize
